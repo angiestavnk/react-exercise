1df5870352908ccf8fb6a878c6d59db05c6b7cb0
Angelina Stavniiciuc <angiestavnk@gmail.com>
2022-10-11 14:44:26 +0300

    create grid layout

---
+++ src/grid/index.js
@@ -0,0 +1,16 @@
+import styles from "./style.module.css";
+
+export const GridPage = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={`${styles.item1} ${styles.item}`} />
+        <div className={`${styles.item2} ${styles.item}`} />
+        <div className={`${styles.item3} ${styles.item}`} />
+        <div className={`${styles.item4} ${styles.item}`} />
+        <div className={`${styles.item5} ${styles.item}`} />
+        <div className={`${styles.item6} ${styles.item}`} />
+      </div>
+    </>
+  )
+};
