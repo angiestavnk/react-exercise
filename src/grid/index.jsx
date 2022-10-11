9c70ac6688e3342a337ccb5ddbfba847f61e3b77
Angelina Stavniiciuc <angiestavnk@gmail.com>
2022-10-11 16:17:31 +0300

    switch between layouts using button

--- src/grid/index.jsx
+++ src/grid/index.jsx
@@ -1,21 +1,38 @@
+import { useEffect, useState } from "react";
 import { COLUMN, GRID } from "../utils/constants";
 import styles from "./style.module.css";
 
 export const GridPage = ({ variant }) => {
+  const [displayState, setDisplay] = useState(variant);
+  const [showButton, setShowButton] = useState(false);
+
+  const changeView = () => {
+    if (displayState === GRID) {
+      setDisplay(COLUMN)
+    } else {
+      setDisplay(GRID)
+    }
+  };
+
   const displayStyle = {
-    display: variant === COLUMN ? "flex" : "grid"
+    display: displayState === COLUMN ? "flex" : "grid"
   };
 
   const itemStyle = {
     item: {
-      width: variant === COLUMN && "250px",
-      height: variant === COLUMN && "250px"
+      width: displayState === COLUMN && "250px",
+      height: displayState === COLUMN && "250px"
     }
   };
 
+  useEffect(() => {
+    setShowButton(true);
+  }, []);
+
   return (
-    <>
-      <div className={styles.container} style={displayStyle}>
+    <div className={styles.container}>
+      {showButton && <button className={styles.btn} onClick={changeView}>Change layout</button>}
+      <div className={styles.contentContainer} style={displayStyle}>
         <div className={`${styles.item1} ${styles.item}`} style={itemStyle.item} />
         <div className={`${styles.item2} ${styles.item}`} style={itemStyle.item} />
         <div className={`${styles.item3} ${styles.item}`} style={itemStyle.item} />
@@ -23,7 +40,7 @@ export const GridPage = ({ variant }) => {
         <div className={`${styles.item5} ${styles.item}`} style={itemStyle.item} />
         <div className={`${styles.item6} ${styles.item}`} style={itemStyle.item} />
       </div>
-    </>
+    </div>
   )
 };
 
