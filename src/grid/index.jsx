abed6957f1a993afc010f1dd3d4a1234fd3096df
Angelina Stavniiciuc <angiestavnk@gmail.com>
2022-10-13 22:02:13 +0300

    change grid layout

--- src/grid/index.jsx
+++ src/grid/index.jsx
@@ -2,8 +2,8 @@ import styles from "./style.module.css";
 
 export const GridPage = () => {
   return (
-    <>
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
         <div className={`${styles.item1} ${styles.item}`} />
         <div className={`${styles.item2} ${styles.item}`} />
         <div className={`${styles.item3} ${styles.item}`} />
@@ -11,6 +11,6 @@ export const GridPage = () => {
         <div className={`${styles.item5} ${styles.item}`} />
         <div className={`${styles.item6} ${styles.item}`} />
       </div>
-    </>
+    </div>
   )
 };
